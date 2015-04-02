88e435295126e20c5631c49d064a6f2d8fa9606d
Kurt Telep <ktelep@gmail.com>
2015-04-02 13:53:14 -0400

    PEP8 Compliance

--- tilt_server.py
+++ tilt_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import os
 import json
 import redis
- 
+
 app = Flask(__name__, static_url_path='/static')
 port = os.getenv('VCAP_APP_PORT', '5000')
 
@@ -12,26 +12,32 @@ r = redis.Redis(host=credentials['hostname'],
                 port=credentials['port'],
                 password=credentials['password'])
 
+
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
+
 @app.route('/send', methods=['POST'])
 def receive_post_data():
     if request.method == 'POST':
-        data_line = request.form['UUID'] + ":" + request.form['TiltLR'] + ":" + request.form['TiltFB'] + ":" + request.form['Direction']
+        data_line = ":".join([request.form['UUID'], request.form['TiltLR']
+                              request.form['TiltFB'], request.form['Direction']
+                              ])
         r.rpush('data_list', data_line)
-        r.rtrim('data_list',0,99)
+        r.rtrim('data_list', 0, 99)
         return "success"
     return "fail"
+
 
 @app.route('/show')
 def show():
     return render_template('dynamic.html')
 
+
 @app.route('/dump')
 def dump_data():
-    return ",".join(map(str,r.lrange('data_list',-50,-1)))
+    return ",".join(map(str, r.lrange('data_list', -50, -1)))
 
 if __name__ == '__main__':
     print "Port" + port

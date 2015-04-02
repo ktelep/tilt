5d760127ad2da447546dd70f4a098e5f74ad95a9
Kurt Telep <ktelep@gmail.com>
2015-04-02 10:10:34 -0400

    Added dynamic test page, updated tilt_server to cleanout redis

--- README.md
+++ README.md
@@ -11,10 +11,7 @@
 
 From the root of the repo
 
-    cf push tilt
+    cf push 
 
-Then bind a redis server to the instance via cloudfoundry.  You may need to restage after you bind the redis instance
-
-Accessing the app via it's URL will present you with an HTML5
-
+Accessing the app via it's URL will present you with an HTML5 page that you can manipulate
 Accessing the app via URL/show will give you a page that provides the most recent data every 500ms
--- tilt_server.py
+++ tilt_server.py
@@ -19,7 +19,7 @@ def index_page():
 @app.route('/send', methods=['POST'])
 def receive_post_data():
     if request.method == 'POST':
-        data_line = request.form['TiltLR'] + ":" + request.form['TiltFB'] + ":" + request.form['Direction']
+        data_line = request.form['UUID'] + ":" request.form['TiltLR'] + ":" + request.form['TiltFB'] + ":" + request.form['Direction']
         r.rpush('data_list', data_line)
         return "success"
     return "fail"
@@ -30,11 +30,12 @@ def show():
 
 @app.route('/show_cool')
 def show_cool():
-    return render_template('cool_display.html')
+    return render_template('dynamic.html')
 
 @app.route('/dump')
 def dump_data():
     return "<br>".join(map(str,r.lrange('data_list',-50,-1)))
+    r.del('data_list')
 
 @app.route('/latest')
 def latest():

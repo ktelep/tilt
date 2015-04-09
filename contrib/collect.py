8960b72fa88d04375c1e9e0cc710a6afd624c3b3
Kurt Telep <ktelep@gmail.com>
2015-04-08 20:09:28 -0400

    Updated collect.py script

--- contrib/collect.py
+++ contrib/collect.py
@@ -2,32 +2,29 @@
 
 import urllib2 as urllib
 import time
+import json
 
-url = 'http://tilt.cfapps.io/dump'
-filename = 'output_file.log'
-max_records = 50
+url = 'http://tilt.cfapps.io/safe_dump?min_score=%d'
+filename = './tilt_output.log'
+max_records = 200 
 
-
-def checksum(st):
-    return reduce(lambda x, y: x+y, map(ord, st))
-
+min_score = 0
 line_count = 0
-checksums = list()
 f = open(filename, 'a')
 
 while True:
     time.sleep(0.5)
     header = {"pragma-directive": "no-cache"}
-    req = urllib.Request(url, headers=header)
+    print url % (min_score,)
+    req = urllib.Request(url % min_score, headers=header)
     page = urllib.urlopen(req)
-    new_data = page.read()
-    new_data_sum = checksum(new_data)
-    if new_data_sum not in checksums:
-        f.write(new_data.replace(",", "\n"))
+    parsed_data = json.loads(page.read())
+    min_score = parsed_data['timestamp']
+    for i in parsed_data['data']:
+        f.write(i)
         f.write("\n")
-        f.flush()
-        checksums.append(new_data_sum)
-        line_count = line_count + 1
+    f.flush()
+    line_count = line_count + 1
     page.close()
     if (line_count > max_records):
         f.close()

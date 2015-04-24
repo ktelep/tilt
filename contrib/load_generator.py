fbd071c3a6e57d6ecf7760d525aa1bccbc15d1e5
Kurt Telep <ktelep@gmail.com>
2015-04-24 10:41:37 -0400

    Added sample load generator

---
+++ contrib/load_generator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import string
+import requests
+import multiprocessing
+from time import sleep
+from random import random, choice
+
+url = "http://tilt.cfapps.io/send"
+#url = "http://localhost:5000/send"
+
+
+def s4(size=4, chars=string.ascii_lowercase + string.digits):
+    return ''.join(choice(chars) for _ in range(size))
+
+
+def guid():
+    return s4()+"-"+s4()
+
+
+def worker():
+    my_id = guid()
+    while True:
+        # Create a fake payload with random data
+        payload = dict(UUID=my_id, TiltLR=random(), TiltFB=random(),
+                       Direction=random(), OS="LoadTest")
+        r = requests.post(url, payload)
+        if r.status_code != 200:
+            print "failed"
+        print r.elapsed
+        sleep(0.05)
+
+if __name__ == '__main__':
+    jobs = []
+    for i in range(10):
+        p = multiprocessing.Process(target=worker)
+        jobs.append(p)
+        p.start()
